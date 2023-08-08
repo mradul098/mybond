4dbae123822f6e3c82902d67cfaaee31aa681572
mradul <mradul098@gmail.com>
2023-08-08 16:06:07 +0530

    cors resolved

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const env=require('dotenv/config');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
